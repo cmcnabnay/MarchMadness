dc2685c27d417a954e1ad968a824af38a8ddad57
cmcnabnay <conor@fleet-lab.com>
2026-06-14 00:31:33 -0700

    initial commit

--- ComputerVision/CV.py
+++ ComputerVision/CV.py
@@ -13,6 +13,9 @@ CAR_WIDTH = 1.8      # meters
 VEHICLE_CLASSES = [2, 3, 5, 7]
 # 2=car, 3=motorcycle, 5=bus, 7=truck
 
+TIME_GAP = 2.0  # seconds
+MPS_TO_MPH = 2.23694
+
 # ---------------------------
 # Load YOLO
 # ---------------------------
@@ -23,9 +26,10 @@ model = YOLO("yolov8s.pt")
 # Open Video
 # ---------------------------
 
-video_path = "dashcam.mp4"
+video_path = "C:\\Users\\cmcna\\Git Repositories\\ComputerVision\\8357-208052048.mp4"
 
 cap = cv2.VideoCapture(video_path)
+print("Video opened:", cap.isOpened())
 
 while cap.isOpened():
 
@@ -70,33 +74,41 @@ while cap.isOpened():
 
         distance = (CAR_WIDTH * FOCAL_LENGTH) / box_width
 
-        label = f"{model.names[cls]}  {distance:.1f} m"
+        recommended_speed_mps = distance / TIME_GAP
+        recommended_speed_mph = recommended_speed_mps * MPS_TO_MPH
+
+        label = (
+            f"{distance:.1f} m | "
+            f"Safe Speed: {recommended_speed_mph:.1f} mph"
+        )
 
         cv2.rectangle(
             frame,
-            (x1, y1),
-            (x2, y2),
-            (0, 255, 0),
+            (20, 20),
+            (450, 120),
+            (0, 0, 0),
+            -1
+        )
+
+        cv2.putText(
+            frame,
+            f"Distance: {distance:.1f} m",
+            (30, 60),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.8,
+            (255,255,255),
             2
         )
 
         cv2.putText(
             frame,
-            label,
-            (x1, y1 - 10),
+            f"Recommended Speed: {recommended_speed_mph:.1f} mph",
+            (30, 100),
             cv2.FONT_HERSHEY_SIMPLEX,
-            0.7,
-            (0, 255, 0),
+            0.8,
+            (255,255,255),
             2
         )
-
-    cv2.line(
-        frame,
-        (frame_center, 0),
-        (frame_center, height),
-        (255, 0, 0),
-        2
-    )
 
     cv2.imshow("Vehicle Distance Estimation", frame)
 
